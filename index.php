99e7599220249e96c425b4a2cef613bef8f98e1c
shailja025 <51818452+shailja025@users.noreply.github.com>
2020-07-14 11:49:45 +0530

    final

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-Welcome
-<br/>
 
-<pre>
-<?php
-
-print "hope you have amazing day" ;
-
-?>
-</pre>
+<HTML>
+<HEAD>
+<title>Launching EC2 instance using terraform</title>
+<body bgcolor=Aqua>
+ HI!!!!!
+<img src="https://shailja85.s3.ap-south-1.amazonaws.com/shailja.jpg" width="1084" height="804">
+</body>
+</HEAD>
+</HTML>
